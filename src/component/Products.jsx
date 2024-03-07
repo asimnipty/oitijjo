3da5eb7fd8b368814d90e5ccb6631677f9221afb
asimnipty <asimnipty@yahoo.com>
2024-03-08 00:45:04 +0600

    filter

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -3,7 +3,16 @@ import { data } from './data'
 
 const Products = () => {
   
-  const [produts, setProducts] = useState(data)
+  const [produts, setProducts] = useState(data);
+  // filter by category
+  const filterProduct = (category) => {
+    setProducts(
+      data.filter((saj) => {
+        return saj.category === category;
+      })
+    );
+  };
+
   
   return (
     <div className=' max-w-[1640px] m-auto px-4 py-12'>
@@ -16,6 +25,7 @@ const Products = () => {
           <div className=' flex justify-between space-x-4'>
             <button className='m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>All</button>
             <button className='m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Clothing</button>
+            <button className='m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Saree</button>
             <button className='m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Health & beauty</button>
             <button className='m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Lifestyle</button>
             <button className='m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Others</button>
@@ -23,10 +33,13 @@ const Products = () => {
         </div>
       </div>
       {/* product display */}
-      <div>
-        {produts.map((saj, index)=> (
-          <div>
-            <img src={saj.image} alt={saj.index}/>
+      <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
+        {produts.map((item, index)=> (
+          <div key={index} className='border rounded-lg shadow-lg hover:scale-105 duration-300'>
+            <img className=' w-full h-[200px] rounded-t-lg' src={item.image} alt={item.index}/>
+            <div>
+              <p className='p-2 font-bold text-blue-950'>{item.name}</p>
+            </div>
           </div>
         ))}
       </div>
--- src/component/data.js
+++ src/component/data.js
@@ -1,29 +1,52 @@
-import img1 from '../assets/products/dress1.jpeg'
+
 export const data = [
     {
         id: 1,
-        name: 'jamdani',
-        category: 'dress',
-        image: {img1},
+        name: 'Saree',
+        category: 'saree',
+        image: require('../assets/products/dress1.jpeg'),
         price: 2000
 
     },
     {
         id: 2,
-        name: 'jamdani',
+        name: 'Kamiz',
         category: 'dress',
-        image: {img1},
+        image: require('../assets/products/dress2.jpeg'),
         price: 2000
 
     },
     {
         id: 3,
-        name: 'jamdani',
+        name: 'Cloth',
         category: 'dress',
-        image: {img1},
+        image: require('../assets/products/dress3.jpeg'),
+        price: 2000
+
+    },
+    {
+        id: 4,
+        name: 'Saree',
+        category: 'saree',
+        image: require('../assets/products/dress4.jpeg'),
+        price: 2000
+
+    },
+    {
+        id: 5,
+        name: 'Saree',
+        category: 'dress',
+        image: require('../assets/products/dress5.jpeg'),
+        price: 2000
+
+    },
+    {
+        id: 6,
+        name: 'Saree',
+        category: 'saree',
+        image: require('../assets/products/dress6.jpeg'),
         price: 2000
 
     }
-
 
 ]
